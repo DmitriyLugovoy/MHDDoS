28676d504aabd0107fb2369ddfaffd2ed27887fd
DmitriyLugovoy <dmitriy.lugovoy.79@gmail.com>
2022-03-27 20:34:42 +0300

    added docker_run.sh

--- _install/docker_run.sh
+++ _install/docker_run.sh
@@ -2,4 +2,4 @@
 
 cd ..
 #docker run --name mhddos -P dyman/battleship_cannon STRESS http://test2.lug.in.ua 0 2 0 10 600 true
-docker run -rm -P dyman/battleship_cannon STRESS http://test2.lug.in.ua 0 2 0 10 600 true
+docker run --rm -P dyman/battleship_cannon STRESS http://test2.lug.in.ua 0 2 0 10 600 true
