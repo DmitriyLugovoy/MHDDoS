269ddbcd991e882eba0cf203a01e11e630651ca7
DmitriyLugovoy <dmitriy.lugovoy.79@gmail.com>
2022-03-28 10:19:41 +0300

    added docker_run.sh

---
+++ _install/install_local.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+cd ..
+pip3 install -r requirements.txt
---
+++ _install/run_local.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+cd ..
+python3 start.py STRESS http://test2.lug.in.ua 0 2 0_proxies_list.txt 10 600 true
